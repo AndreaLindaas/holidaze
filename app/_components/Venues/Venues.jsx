a97d0cc5174231bd2763b749cebe965efd8b6a70
AndreaLindaas <112169120+AndreaLindaas@users.noreply.github.com>
2024-04-10 14:05:33 +0200

    startet fetching venues

--- app/_components/Venues/Venues.jsx
+++ app/_components/Venues/Venues.jsx
@@ -1,5 +1,29 @@
-import React from "react";
+"use client";
+// import { API_URL } from "@/app/_lib/constants";
+import React, { useEffect, useState } from "react";
 
 export default function Venues() {
-  return <div></div>;
+  const [venues, setVenues] = useState([]);
+  const [showError, setShowError] = useState(false);
+
+  useEffect(() => {
+    fetch("https://api.noroff.dev/api/v1/holidaze/venues")
+      .then((response) => response.json())
+      .then((result) => {
+        setVenues(result);
+      })
+      .catch((error) => {
+        setShowError(true);
+      });
+  }, []);
+  console.log(venues);
+  const showVenues = () => {
+    return venues.map((venue) => {
+      return <div key={venue.id}>{venue.name}</div>;
+    });
+  };
+  //   if (showError) {
+  //     return <div className="error-message">Problem with fetching Venues.</div>;
+  //   }
+  return <div>{showVenues()}</div>;
 }
---
+++ app/_lib/constants.js
@@ -0,0 +1,2 @@
+export const API_URL =
+  import.meta.env.VITE_API_URL || "https://api.noroff.dev/api/v1/holidaze";
