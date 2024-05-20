8ac8e274f4a2500efc64220358af8b00fd20058e
AndreaLindaas <112169120+AndreaLindaas@users.noreply.github.com>
2024-05-20 20:58:51 +0200

    added code to search

--- app/search/[[...searchWord]]/page.jsx
+++ app/search/[[...searchWord]]/page.jsx
@@ -1,9 +1,14 @@
 "use client";
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import Search from "../../_components/Search/Search";
 import useSearch from "../../_hooks/fetchSearch";
 import VenueCard from "../../_components/VenueCard/VenueCard";
+import Link from "next/link";
+import styles from "./Search.module.scss";
 export default function SearchPage(props) {
+  const [numberOfGuests, setNumberOfGuests] = useState(2);
+  const [maxPrice, setMaxPrice] = useState(0);
+
   const { searchWord } = props.params;
   const { isLoading: isLoadingSearchData, data: search } =
     useSearch(searchWord);
@@ -11,11 +16,24 @@ export default function SearchPage(props) {
   const showSearchResults = () => {
     if (searchWord) {
       if (!isLoadingSearchData && search.data) {
-        return search.data.map((venue, i) => {
+        const filtered = search.data.filter((venue) => {
+          if (maxPrice > 0) {
+            return venue.maxGuests >= numberOfGuests && venue.price <= maxPrice;
+          }
+          return venue.maxGuests >= numberOfGuests;
+        });
+
+        if (filtered.length === 0) {
           return (
-            <div key={venue.id}>
+            <div>No hits. Please try a new search or adjust your filters.</div>
+          );
+        }
+
+        return filtered.map((venue) => {
+          return (
+            <Link href={`/venue/${venue.id}`} key={venue.id}>
               <VenueCard venue={venue} />
-            </div>
+            </Link>
           );
         });
       }
@@ -24,7 +42,7 @@ export default function SearchPage(props) {
   return (
     <div>
       <Search searchWord={searchWord} />
-      <div> {showSearchResults()}</div>
+      <div className={styles.venueCardContainer}> {showSearchResults()}</div>
     </div>
   );
 }
