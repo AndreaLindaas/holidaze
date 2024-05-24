e5b7ec5d50bf8fee304f1ec629a812eb5784cc82
AndreaLindaas <112169120+AndreaLindaas@users.noreply.github.com>
2024-05-24 20:41:14 +0200

    removed unused code

--- app/_components/Providers.js
+++ app/_components/Providers.js
@@ -1,15 +1,9 @@
 "use client";
-import { LocalizationProvider } from "@mui/x-date-pickers";
-import { AdapterMoment } from "@mui/x-date-pickers/AdapterMoment";
 import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
 
 export default function Providers({ children }) {
   const queryClient = new QueryClient();
   return (
-    <QueryClientProvider client={queryClient}>
-      <LocalizationProvider dateAdapter={AdapterMoment}>
-        {children}
-      </LocalizationProvider>
-    </QueryClientProvider>
+    <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
   );
 }
